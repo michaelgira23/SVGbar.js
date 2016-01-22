7e3c774691ed0c9bc77ea174d9c14fa5d6e1bdc0
Michael <michaelgira23@gmail.com>
2016-01-22 11:09:24 -0600

    Nick, you can't have parenthesis while using a function as a parameter

--- SVGbar.js
+++ SVGbar.js
@@ -120,7 +120,7 @@
     }
     
     updateProgress();
-    setInterval(updateProgress(), 500);
+    setInterval(updateProgress, 500);
     
     function distance(x1, y1, x2, y2) {
         return Math.sqrt( Math.pow((x2-x1),2) + Math.pow((y2-y1),2) );
